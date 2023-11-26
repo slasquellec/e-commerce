f654a530c7c862d1b0ec093683e041b06b68fe06
slasquellec <126764526+slasquellec@users.noreply.github.com>
2023-11-26 12:39:13 +0100

    add form and form template personalization

---
+++ assets/showAndHidePassword.js
@@ -0,0 +1,17 @@
+const eyeOn = document.querySelector('.eye-on');
+const eyeOff = document.querySelector('.eye-off');
+const inputPassword = document.querySelector('#inputPassword');
+
+eyeOff.style.display = "none";
+
+eyeOn.addEventListener('click', () => {
+    eyeOn.style.display = "none";
+    eyeOff.style.display = "block";
+    inputPassword.type = "text";
+});
+
+eyeOff.addEventListener('click', () => {
+    eyeOn.style.display = "block";
+    eyeOff.style.display = "none";
+    inputPassword.type = "password";
+});
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +14,10 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('name')
-            ->add('firstname')
-            ->add('isVerified')
+            ->add('email', HiddenType::class)
+            ->add('message', TextareaType::class, [
+                'label' => 'Message'
+            ])
         ;
     }
 
